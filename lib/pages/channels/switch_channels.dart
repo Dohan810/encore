7b650363bcf9554b16129a72f0a9a678dedaa29d
dohan19990727 <dohan19992707@gmail.com>
2021-08-18 12:39:33 +0200

    Added the custom seach widget

--- lib/pages/channels/switch_channels.dart
+++ lib/pages/channels/switch_channels.dart
@@ -6,6 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:my_encore/colors/colors.dart';
 import 'package:my_encore/models/names.dart';
 import 'package:my_encore/widgets/appbar.dart';
+import 'package:my_encore/widgets/input_bar.dart';
 
 class SwitchChannels extends StatefulWidget {
   SwitchChannels({Key key}) : super(key: key);
@@ -27,7 +28,7 @@ class _SwitchChannelsState extends State<SwitchChannels> {
       "assets/4.png",
     ];
 
-    switchChannels() {
+    List<Widget> switchChannels() {
       List<Widget> temp = [];
       tempImages.forEach(
         (element) {
@@ -122,62 +123,8 @@ class _SwitchChannelsState extends State<SwitchChannels> {
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.050,
           ),
-          Center(
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.080,
-              width: MediaQuery.of(context).size.width * 0.90,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(
-                  color: Colors.blue,
-                  width: 2.0,
-                ),
-              ),
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(7, 0, 0, 0),
-                      child: Text('Search Channels',
-                          style: GoogleFonts.poppins(
-                            textStyle: TextStyle(
-                              fontSize:
-                                  13 * MediaQuery.textScaleFactorOf(context),
-                              fontWeight: FontWeight.w500,
-                              color: AppColors.kBlue,
-                            ),
-                          )),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(150, 0, 0, 0),
-                      child: InkWell(
-                        onTap: () {},
-                        child: Container(
-                          height: MediaQuery.of(context).size.height * 0.060,
-                          width: MediaQuery.of(context).size.width * 0.13,
-                          decoration: BoxDecoration(
-                            color: AppColors.kBlue,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(7),
-                              topRight: Radius.circular(7),
-                              bottomLeft: Radius.circular(7),
-                              bottomRight: Radius.circular(7),
-                            ),
-                          ),
-                          child: Icon(
-                            Icons.search,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+          CustomInputBar(
+            title: "Search Channels",
           ),
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.020,
---
+++ lib/widgets/input_bar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_encore/colors/colors.dart';
+
+class CustomInputBar extends StatefulWidget {
+  final String title;
+  CustomInputBar({this.title = ""});
+
+  @override
+  _CustomInputBarState createState() => _CustomInputBarState();
+}
+
+class _CustomInputBarState extends State<CustomInputBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.080,
+        width: MediaQuery.of(context).size.width * 0.90,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: Colors.blue,
+            width: 2.0,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(7, 0, 0, 0),
+                child: Text(widget.title,
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        fontSize: 13 * MediaQuery.textScaleFactorOf(context),
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.kBlue,
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(150, 0, 0, 0),
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.060,
+                    width: MediaQuery.of(context).size.width * 0.13,
+                    decoration: BoxDecoration(
+                      color: AppColors.kBlue,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(7),
+                        topRight: Radius.circular(7),
+                        bottomLeft: Radius.circular(7),
+                        bottomRight: Radius.circular(7),
+                      ),
+                    ),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
